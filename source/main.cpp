4c7740f11d09ba2115f0e02c39c7c9a12603f0b3
Stefan <47391841+ostef@users.noreply.github.com>
2023-04-04 17:32:52 +0200

    Initialize ImGui, show demo window

--- source/main.cpp
+++ source/main.cpp
@@ -29,7 +29,7 @@ int main (int argc, const char **args)
 	glfwWindowHint (GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint (GLFW_SAMPLES, 4);
 
-	auto window = glfwCreateWindow (640, 480, "ft_minecraft", null, null);
+	auto window = glfwCreateWindow (1280, 720, "ft_minecraft", null, null);
 	if (!window)
 		return 1;
 
@@ -41,23 +41,42 @@ int main (int argc, const char **args)
 	int gl_version = gladLoadGL (cast (GLADloadfunc) glfwGetProcAddress);
 	println ("GL Version %d.%d", GLAD_VERSION_MAJOR (gl_version), GLAD_VERSION_MINOR (gl_version));
 
+	IMGUI_CHECKVERSION ();
+	ImGui::CreateContext ();
+	auto io = ImGui::GetIO ();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+	ImGui::StyleColorsDark ();
+
+	ImGui_ImplGlfw_InitForOpenGL (window, true);
+	ImGui_ImplOpenGL3_Init ("#version 130");
+
 	glEnable (GL_MULTISAMPLE);
 	glClearColor (0.2, 0.3, 0.6, 1.0);	// Sky color
 
+	bool show_demo_window = true;
 	while (!glfwWindowShouldClose (window))
 	{
 		arena_reset (&frame_arena);
 
 		glfwPollEvents ();
 
+		ImGui_ImplOpenGL3_NewFrame ();
+		ImGui_ImplGlfw_NewFrame ();
+		ImGui::NewFrame ();
+
+		if (show_demo_window)
+			ImGui::ShowDemoWindow (&show_demo_window);
+
+		ImGui::Render ();
 		int width, height;
 		glfwGetFramebufferSize (window, &width, &height);
 		glViewport (0, 0, width, height);
 		glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData (ImGui::GetDrawData ());
 
 		glfwSwapBuffers (window);
-
-		sleep_milliseconds (10);
 	}
 
 	return 0;
