800e1a77bd888f816c09ae5f16f2d430f7bdc119
Stefan <47391841+ostef@users.noreply.github.com>
2023-04-06 18:04:08 +0200

    Define IM_ASSERT to prevent ImGui from overwriting
    Our assert with the one from assert.h

--- source/Minecraft.hpp
+++ source/Minecraft.hpp
@@ -5,7 +5,10 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
 
+// We need to define IM_ASSERT otherwise ImGui will overwrite
+// our own with assert from ucrt/assert.h
+#define IM_ASSERT assert
 #include "ImGui.hpp"
 
-extern Arena frame_arena;
+extern Arena     frame_arena;
 extern Allocator frame_allocator;
