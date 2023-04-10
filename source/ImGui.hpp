e2ab8f663550eb6e076e8946d6c5706cd0b73d21
Stefan <47391841+ostef@users.noreply.github.com>
2023-04-10 17:56:20 +0200

    Use stb sprintf in ImGui

--- source/ImGui.hpp
+++ source/ImGui.hpp
@@ -1,5 +1,43 @@
 #pragma once
 
+#define IMGUI_DISABLE_DEFAULT_FORMAT_FUNCTIONS
+
+// We do our own stb sprintf wrapper because ImGui includes
+// the implementation of stb sprintf if we define IMGUI_USE_STB_SPRINTF
+// We already implement stb sprintf ourselves in Core.cpp so this causes problems
+
+#include <stb_sprintf.h>
+
+inline
+int ImFormatString(char* buf, size_t buf_size, const char* fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+
+    int w = stbsp_vsnprintf(buf, (int)buf_size, fmt, args);
+
+    va_end(args);
+    if (buf == NULL)
+        return w;
+    if (w == -1 || w >= (int)buf_size)
+        w = (int)buf_size - 1;
+    buf[w] = 0;
+    return w;
+}
+
+inline
+int ImFormatStringV(char* buf, size_t buf_size, const char* fmt, va_list args)
+{
+    int w = stbsp_vsnprintf(buf, (int)buf_size, fmt, args);
+
+    if (buf == NULL)
+        return w;
+    if (w == -1 || w >= (int)buf_size)
+        w = (int)buf_size - 1;
+    buf[w] = 0;
+    return w;
+}
+
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include <misc/single_file/imgui_single_file.h>
 
